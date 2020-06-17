7bfabd9e9913637865e5c177b62d541f40ce3d38
Marcos Gabriel <mpgabriel1972@gmail.com>
2020-06-17 11:38:49 -0300

    Verify Password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,30 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Bem vindo a sua área secreta"
-  })
+  const secretContent = `
+  <h3>Bem vindo a área secreta</h3>
+  <p>Aqui sua Festa é mais <strong>Divertida</strong>, e nao vemos a hora de nos encontrar.</p>
+  `
+
+  let body
+
+  if(event.body){
+    body = JSON.parse(event.body)
+
+  }else{
+    body = {}
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+  } else{ 
+    callback(null, {
+      statusCode: 401
+      
+    })
+  }
+  
+  
 
 }
