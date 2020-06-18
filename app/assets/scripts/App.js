2ae5c0d4481c44fbb15fb8e143c45613eb777941
Marcos Gabriel <mpgabriel1972@gmail.com>
2020-06-18 09:54:13 -0300

    included ReactJS tests frontend

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,6 +6,18 @@ import StickyHeader from './modules/StickyHeader'
 import ClientArea from './modules/ClientArea'
 
 
+
+
+//  React Rlated code Code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//Import React components that we created
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+
+
 new ClientArea()
 new StickyHeader()
 
